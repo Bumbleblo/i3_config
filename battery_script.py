fad756fc4d3450958ce8065fa750355d10c35e68
bumbleblo <bumbleblo2013@gmail.com>
2018-07-28 05:20:28 -0300

    add initial low battery alert script

--- battery_script.py
+++ battery_script.py
@@ -1,24 +1,30 @@
+#! /usr/bin/python3
+
 """
 Wait SLEEP_TIME seconds and check battery level, if is
 less than LOWER_LEVEL critical message in printed.
 """
-
 import psutil # dependence 
 import time
 import subprocess
+import logging
 
-# seconds in sleep
+# minutes in sleep
 SLEEP_TIME = 5
 LOWER_LEVEL = 15
 
 if __name__ == '__main__':
 	
-	battery = psutil.sensors_battery()
+	logging.debug('Battery script up')
 
+	battery = psutil.sensors_battery()
+	
 	while(1):
+
 		if battery.percent <= LOWER_LEVEL:
 			subprocess.Popen(['notify-send', '-u', 'critical', 'Low Battery', 'Charge you laptop'])
 
 		time.sleep(SLEEP_TIME*1000)
 	
+	logging.debug('Battery script down')
 	
